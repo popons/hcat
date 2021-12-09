5ad847553351eb632b38cfd7ef98abb200d28a08
Yutaka Furukawa <frkw@fuse.co.jp>
2021-12-09 23:46:36 +0900

    fix: 存在するファイルのみをヘッダに含めるようにした。

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,34 @@ use std::path::Path;
 fn main() -> Result<(), Error> {
     let files = args().skip(1).collect::<Vec<String>>();
     let lines_coll = to_lines_collection(&files);
-    print_header(files);
-    print_body(lines_coll);
+    let files_ = lines_coll.iter().map(|(a, _)| *a).collect::<Vec<&String>>();
+    let bodies = lines_coll
+        .iter()
+        .map(|(_, b)| b)
+        .collect::<Vec<&Vec<String>>>();
+    print_header(files_);
+    print_body(bodies);
     Ok(())
 }
 
-fn to_lines(path: impl AsRef<Path>) -> Result<Vec<String>, Error> {
-    let f = File::open(path)?;
+fn to_lines<P: AsRef<Path>>(path: P) -> Result<(P, Vec<String>), Error> {
+    let f = File::open(&path)?;
     let r = BufReader::new(f);
     let lines = r.lines();
-    Ok(lines.collect::<Result<Vec<String>, Error>>()?)
+    let xs = lines.collect::<Result<Vec<String>, Error>>()?;
+    Ok((path, xs))
 }
 
-fn to_lines_collection(files: &Vec<String>) -> Vec<Vec<String>> {
+fn to_lines_collection(files: &Vec<String>) -> Vec<(&String, Vec<String>)> {
     let lines_coll = files
         .iter()
         .map(|x| to_lines(x))
         .filter_map(Result::ok)
-        .collect::<Vec<Vec<String>>>();
+        .collect::<Vec<(&String, Vec<String>)>>();
     lines_coll
 }
 
-fn print_body(lines_coll: Vec<Vec<String>>) {
+fn print_body(lines_coll: Vec<&Vec<String>>) {
     let empty = "".to_string();
     for i in 0.. {
         if lines_coll.iter().all(|x| x.get(i) == None) {
@@ -44,7 +50,7 @@ fn print_body(lines_coll: Vec<Vec<String>>) {
     }
 }
 
-fn print_header(files: Vec<String>) {
+fn print_header(files: Vec<&String>) {
     for (i, x) in files.iter().enumerate() {
         if i != 0 {
             print!(",");
