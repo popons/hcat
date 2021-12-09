de81e2cf0e66f8b41ebb3740870b3a952008fb05
Yutaka Furukawa <frkw@fuse.co.jp>
2021-12-10 00:40:12 +0900

    fix: clapで色々オプション化した

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
-use std::env::args;
+use clap::Parser;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
+use std::io::{self, BufRead, BufReader, Error};
 use std::path::Path;
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct Args {
+    /// Names of read files
+    files: Vec<String>,
+
+    /// Number of skip line from head
+    #[clap(short, long, default_value_t = 0)]
+    skip: usize,
+
+    /// print header as file name
+    #[clap(short, long)]
+    head: bool,
+
+    /// Separator when print items
+    #[clap(short, long, default_value = ",")]
+    sep: String,
+}
+
 fn main() -> Result<(), Error> {
-    let files = args().skip(1).collect::<Vec<String>>();
+    let args = Args::parse();
+    let sep = &args.sep;
+    let files = args.files;
     let lines_coll_ = to_lines_collection(&files);
     let files_: Vec<&String> = lines_coll_.iter().map(|(a, _)| *a).collect();
     let lines_coll: Vec<&Vec<String>> = lines_coll_.iter().map(|(_, b)| b).collect();
-    print_header(&files_);
-    print_body(&lines_coll);
+    if args.head {
+        print_header(&files_, sep);
+    }
+    print_body(&lines_coll, args.skip, sep);
     Ok(())
 }
 
@@ -29,15 +53,15 @@ fn to_lines_collection(files: &Vec<String>) -> Vec<(&String, Vec<String>)> {
         .collect()
 }
 
-fn print_body(lines_coll: &Vec<&Vec<String>>) {
+fn print_body(lines_coll: &Vec<&Vec<String>>, skip: usize, sep: &String) {
     let empty = "".to_string();
-    for i in 0.. {
+    for i in skip.. {
         if lines_coll.iter().all(|x| x.get(i) == None) {
             break;
         }
         for (j, x) in lines_coll.iter().enumerate() {
             if j != 0 {
-                print!(",");
+                print!("{}", sep);
             }
             let v = x.get(i);
             print!("{}", v.unwrap_or(&empty));
@@ -46,10 +70,10 @@ fn print_body(lines_coll: &Vec<&Vec<String>>) {
     }
 }
 
-fn print_header(files: &Vec<&String>) {
+fn print_header(files: &Vec<&String>, sep: &String) {
     for (i, x) in files.iter().enumerate() {
         if i != 0 {
-            print!(",");
+            print!("{}", sep);
         }
         print!("{}", x);
     }
