c0edeeefb3e5207c81b3d34e8270c3bd173987a0
Yutaka Furukawa <frkw@fuse.co.jp>
2021-12-09 23:58:25 +0900

    fix: 別にmoveじゃなくて貸し出すだけで良い部分は貸し出す

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,10 @@ use std::path::Path;
 fn main() -> Result<(), Error> {
     let files = args().skip(1).collect::<Vec<String>>();
     let lines_coll_ = to_lines_collection(&files);
-    let files_ = lines_coll_
-        .iter()
-        .map(|(a, _)| *a)
-        .collect::<Vec<&String>>();
-    let lines_coll = lines_coll_
-        .iter()
-        .map(|(_, b)| b)
-        .collect::<Vec<&Vec<String>>>();
-    print_header(files_);
-    print_body(lines_coll);
+    let files_: Vec<&String> = lines_coll_.iter().map(|(a, _)| *a).collect();
+    let lines_coll: Vec<&Vec<String>> = lines_coll_.iter().map(|(_, b)| b).collect();
+    print_header(&files_);
+    print_body(&lines_coll);
     Ok(())
 }
 
@@ -32,10 +26,10 @@ fn to_lines_collection(files: &Vec<String>) -> Vec<(&String, Vec<String>)> {
         .iter()
         .map(|x| to_lines(x))
         .filter_map(Result::ok)
-        .collect::<Vec<(&String, Vec<String>)>>()
+        .collect()
 }
 
-fn print_body(lines_coll: Vec<&Vec<String>>) {
+fn print_body(lines_coll: &Vec<&Vec<String>>) {
     let empty = "".to_string();
     for i in 0.. {
         if lines_coll.iter().all(|x| x.get(i) == None) {
@@ -52,7 +46,7 @@ fn print_body(lines_coll: Vec<&Vec<String>>) {
     }
 }
 
-fn print_header(files: Vec<&String>) {
+fn print_header(files: &Vec<&String>) {
     for (i, x) in files.iter().enumerate() {
         if i != 0 {
             print!(",");
